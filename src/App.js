3a06925afaafa8a7e056a6cacbf62f18fc111de6
Galo Canizares <galo.canizares@gmail.com>
2020-12-28 15:23:51 -0600

    added new shapes

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 import { TextureLoader } from 'three/src/loaders/TextureLoader'
 import uuid from "short-uuid"
 
-import './index.css'
 import floorImg from './floor.jpg'
 import floorBmp from './floorn.jpg'
 import shape from './shape.gltf'
+import orb from './orb.gltf'
 
 extend({ OrbitControls })
 
@@ -36,59 +36,41 @@ const CameraControls = () => {
 }
 
 function Shapes(props) {
-  const [items, set] = useState([])
+  const [items, setItems] = useState([])
+  //const [click, toggle] = useState(true)
   //setTimeout(() => set(items => [...items, uuid.generate()]), 1000)
   const handleClick = useCallback(e => {
     e.stopPropagation()
     //delta is difference from clicked point
     //console.log(e.delta)
     if (e.delta < 2){
-      set(items => [...items, uuid.generate()])
+      setItems(items => [...items, uuid.generate()])
     }
   }, [])
   const pos = useRef()
 
   return (
     <>
-    <mesh
+    <group
     onClick = {handleClick}
     onPointerMove={e => {
-      pos.current = [e.intersections[0].point.x, e.intersections[0].point.y + 5, e.intersections[0].point.z]
-    }}
-    >
-    <planeGeometry attach="geometry" args={[50, 50]} />
-    <meshBasicMaterial transparent opacity={0}/>
-    </mesh>
-    {items.map((key) => (
-        <Cube key={key} position={pos.current} />
+      e.stopPropagation()
+      pos.current = [e.intersections[0].point.x, e.intersections[0].point.y+5, e.intersections[0].point.z]
+    }}>
+      <mesh>
+        <planeGeometry attach="geometry" args={[50, 50]} />
+        <meshBasicMaterial color='white' transparent opacity={0}/>
+      </mesh>
+        <mesh rotation={[-Math.PI / 2, 0, 0]} position={[0, 0.2, 0]}>
+        <planeGeometry attach="geometry" args={[50, 50]} />
+        <meshBasicMaterial color='white' transparent opacity={0}/>
+      </mesh>
+    </group>
+    {items.map((key, index) => (
+      index % 2 === 0 ? (<Orb key={key} position={pos.current} />)
+      : (<Cube key={key} position={pos.current} />)
       ))}
     </>
-  )
-}
-
-// function Shapes(props) {
-//   return (
-//     <Cube position={[4, 4, 0]}/>
-//   )
-// }
-
-function Plane(props) {
-  const [ref] = usePlane(() => ({ rotation: [-Math.PI / 2, 0, 0],...props}))
-
-  const floor = useLoader(TextureLoader, floorImg)
-  const floorB = useLoader(TextureLoader, floorBmp)
-
-  return (
-    <mesh
-    ref={ref}
-    receiveShadow
-    //onClick = {handleClick}
-    onPointerMove={e => {
-      //pos.current = [e.intersections[0].point.x, e.intersections[0].point.y + 5, e.intersections[0].point.z]
-    }}>
-      <planeGeometry attach="geometry" args={[50, 50]} />
-      <meshPhongMaterial attach="material" map={floor} bumpMap={floorB} color='white' />
-    </mesh>
   )
 }
 
@@ -104,7 +86,6 @@ function Cube(props) {
    }))
   const [hovered, setHover] = useState(false)
   //const [active, setActive] = useState(false)
-
   useFrame(state => {
     if (hovered === true){
       api.applyForce([0, 10, 0], [-state.mouse.x, -state.mouse.y, 0])
@@ -123,18 +104,75 @@ function Cube(props) {
       onPointerOut={(event) => setHover(false)}>
       <meshPhongMaterial
         attach="material"
-        color={hovered ? '#'+(Math.random()*0xFFFFFF<<0).toString(16): 'orange'}
+        color={hovered ? '#'+Math.floor(Math.random()*16777215).toString(16): 'orange'}
        />
     </mesh>
     </>
   )
 }
 
-export default function() {
-  //const [show, toggle] = useState(true)
+function Orb(props) {
+  const gltf = useLoader(GLTFLoader, orb)
+
+  const [ref, api] = useBox(() => ({
+    mass: 1,
+    args: [2, 2, 2],
+    position: [0, 5, 0],
+    rotation: [0, 0, 0],
+     ...props
+   }))
+  const [hovered, setHover] = useState(false)
+
+  useFrame(state => {
+    if (hovered === true){
+      api.applyForce([0, 10, 0], [-state.mouse.x, -state.mouse.y, 0])
+    }
+  })
+
+  return (
+    <>
+    <mesh
+      receiveShadow
+      castShadow
+      ref={ref}
+      geometry = {gltf.nodes.Orb.geometry }
+      onPointerOver={(event) => setHover(true)}
+      onPointerOut={(event) => setHover(false)}>
+      <meshPhongMaterial
+        attach="material"
+        color={hovered ? '#'+Math.floor(Math.random()*16777215).toString(16): 'orange'}
+       />
+    </mesh>
+    </>
+  )
+}
+
+function Plane(props) {
+  const [ref] = usePlane(() => ({ rotation: [-Math.PI / 2, 0, 0],...props}))
+  const floor = useLoader(TextureLoader, floorImg)
+  const floorB = useLoader(TextureLoader, floorBmp)
+
+  return (
+    <mesh ref={ref} receiveShadow>
+      <planeGeometry attach="geometry" args={[60, 60]} />
+      <meshPhongMaterial attach="material" map={floor} bumpMap={floorB} color='white' />
+    </mesh>
+  )
+}
+
+export default function App() {
+  const [show, toggleShow] = useState(true)
+  const clearToys = (e) => {
+    toggleShow(!show)
+  }
   //setTimeout(() => toggle(!show), 1000)
   return (
   <>
+    <div className='nav'>
+      <div className='button'>
+        <h1 onPointerDown={clearToys} onPointerUp={clearToys}>CLEAR</h1>
+      </div>
+    </div>
     <Canvas
     shadowMap gl={{ alpha: false }}
     orthographic camera={{ zoom: 50, position: [0, 100, 50]}}>
@@ -144,12 +182,13 @@ export default function() {
       <spotLight position={[30, 30, 30]} angle={0.3} penumbra={1} intensity={1} castShadow />
       <Physics>
       <Suspense fallback={null}>
-        <Shapes />
+        {show && <Shapes />}
         <Plane />
         <Cube />
         </Suspense>
       </Physics>
     </Canvas>
+
     </>
   )
 }
